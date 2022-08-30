ce1859c6d6923af04daebd080ba9208b2a1505d7
Majd Sheikh <majd_sheikh@hotmail.com>
2022-08-30 22:12:39 +0300

    likes challenege

---
+++ likes challenge/likeschallenge2.js
@@ -0,0 +1,33 @@
+function incrementButton1() {
+    var element = document.getElementById("incrementText1");
+    var value = element.innerHTML;
+    
+    ++value;
+    
+    console.log(value);
+    document.getElementById("incrementText1").innerHTML = value;
+    }
+
+
+
+    function incrementButton2() {
+        var element = document.getElementById("incrementText2");
+        var value = element.innerHTML;
+        
+        ++value;
+        
+        console.log(value);
+        document.getElementById("incrementText2").innerHTML = value;
+        }
+
+   
+    function incrementButton3() {
+        var element = document.getElementById("incrementText3");
+        var value = element.innerHTML;
+            
+        ++value;
+            
+        console.log(value);
+         document.getElementById("incrementText3").innerHTML = value;
+            }
+
