e880c22f2748b264c265788d2e59cdc344b75428
Majd Sheikh <majd_sheikh@hotmail.com>
2022-08-31 21:17:44 +0300

    Dojo Weather Assignment

---
+++ DojoWeather/DojoWeather.js
@@ -0,0 +1,74 @@
+
+function message() {
+    alert("Loading weather report...");
+}
+
+
+function hide() {
+    var element = document.querySelector(".lastbox");
+    element.remove();  
+}
+
+    function CelToFeh() {
+        var element = document.getElementById("number");
+        var cTemp = element.innerHTML;
+        var value = cTemp * 9 / 5 + 32;
+        console.log(value);
+        document.getElementById("number").innerHTML = value;
+}
+function CelToFeh1() {
+    var element = document.getElementById("number1");
+    var cTemp = element.innerHTML;
+    var value = cTemp * 9 / 5 + 32;
+    console.log(value);
+    document.getElementById("number1").innerHTML = value;
+}
+function CelToFeh2() {
+    var element = document.getElementById("number2");
+    var cTemp = element.innerHTML;
+    var value = cTemp * 9 / 5 + 32;
+    console.log(value);
+    document.getElementById("number2").innerHTML = value;
+}
+function CelToFeh3() {
+    var element = document.getElementById("number3");
+    var cTemp = element.innerHTML;
+    var value = cTemp * 9 / 5 + 32;
+    console.log(value);
+    document.getElementById("number3").innerHTML = value;
+}
+function CelToFeh4() {
+    var element = document.getElementById("number4");
+    var cTemp = element.innerHTML;
+    var value = cTemp * 9 / 5 + 32;
+    console.log(value);
+    document.getElementById("number4").innerHTML = value;
+}
+function CelToFeh5() {
+    var element = document.getElementById("number5");
+    var cTemp = element.innerHTML;
+    var value = cTemp * 9 / 5 + 32;
+    console.log(value);
+    document.getElementById("number5").innerHTML = value;
+}
+function CelToFeh5() {
+    var element = document.getElementById("number5");
+    var cTemp = element.innerHTML;
+    var value = cTemp * 9 / 5 + 32;
+    console.log(value);
+    document.getElementById("number5").innerHTML = value;
+}
+function CelToFeh6() {
+    var element = document.getElementById("number6");
+    var cTemp = element.innerHTML;
+    var value = cTemp * 9 / 5 + 32;
+    console.log(value);
+    document.getElementById("number6").innerHTML = value;
+}
+function CelToFeh7() {
+    var element = document.getElementById("number7");
+    var cTemp = element.innerHTML;
+    var value = cTemp * 9 / 5 + 32;
+    console.log(value);
+    document.getElementById("number7").innerHTML = value;
+}
