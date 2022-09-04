5f041e9dd788736d37354316695a03b75469e6d5
Majd Sheikh <majd_sheikh@hotmail.com>
2022-09-04 16:40:19 +0300

    Edit on this Assignment

--- DojoWeather/DojoWeather.js
+++ DojoWeather/DojoWeather.js
@@ -11,64 +11,12 @@ function hide() {
 
     function CelToFeh() {
         var element = document.getElementById("number");
-        var cTemp = element.innerHTML;
-        var value = cTemp * 9 / 5 + 32;
-        console.log(value);
-        document.getElementById("number").innerHTML = value;
-}
-function CelToFeh1() {
-    var element = document.getElementById("number1");
-    var cTemp = element.innerHTML;
-    var value = cTemp * 9 / 5 + 32;
-    console.log(value);
-    document.getElementById("number1").innerHTML = value;
-}
-function CelToFeh2() {
-    var element = document.getElementById("number2");
-    var cTemp = element.innerHTML;
-    var value = cTemp * 9 / 5 + 32;
-    console.log(value);
-    document.getElementById("number2").innerHTML = value;
-}
-function CelToFeh3() {
-    var element = document.getElementById("number3");
-    var cTemp = element.innerHTML;
-    var value = cTemp * 9 / 5 + 32;
-    console.log(value);
-    document.getElementById("number3").innerHTML = value;
-}
-function CelToFeh4() {
-    var element = document.getElementById("number4");
-    var cTemp = element.innerHTML;
-    var value = cTemp * 9 / 5 + 32;
-    console.log(value);
-    document.getElementById("number4").innerHTML = value;
-}
-function CelToFeh5() {
-    var element = document.getElementById("number5");
-    var cTemp = element.innerHTML;
-    var value = cTemp * 9 / 5 + 32;
-    console.log(value);
-    document.getElementById("number5").innerHTML = value;
-}
-function CelToFeh5() {
-    var element = document.getElementById("number5");
-    var cTemp = element.innerHTML;
-    var value = cTemp * 9 / 5 + 32;
-    console.log(value);
-    document.getElementById("number5").innerHTML = value;
-}
-function CelToFeh6() {
-    var element = document.getElementById("number6");
-    var cTemp = element.innerHTML;
-    var value = cTemp * 9 / 5 + 32;
-    console.log(value);
-    document.getElementById("number6").innerHTML = value;
-}
-function CelToFeh7() {
-    var element = document.getElementById("number7");
-    var cTemp = element.innerHTML;
-    var value = cTemp * 9 / 5 + 32;
-    console.log(value);
-    document.getElementById("number7").innerHTML = value;
+        var cTemp = document.querySelectorAll("#number") //array of spans#number, length = 8
+        console.log(cTemp);
+        for (var i = 0; i<cTemp.length; i++) {
+            var value = (cTemp[i].innerHTML * 9 / 5) + 32
+            console.log(value);
+            cTemp[i].innerHTML = value;
+        }
+        
 }
